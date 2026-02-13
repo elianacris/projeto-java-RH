b8337015c563edc35efc71b226634004992643d7
eliana.santos <eliana.santos@postalis.org.br>
2026-02-13 14:57:27 -0300

    test: enable test profile for application tests

--- rh-system/src/test/java/br/org/postalis/training/rh/RhSystemApplicationTests.java
+++ rh-system/src/test/java/br/org/postalis/training/rh/RhSystemApplicationTests.java
@@ -2,8 +2,10 @@ package br.org.postalis.training.rh;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class RhSystemApplicationTests {
 
 	@Test
