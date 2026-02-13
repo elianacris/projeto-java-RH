ea0bbd2472b52cdc65c34fbd857d409e266fdd57
eliana.santos <eliana.santos@postalis.org.br>
2026-02-13 13:36:24 -0300

    test: adicionar classe de teste base do Spring Boot

---
+++ rh-system/src/test/java/br/org/postalis/training/rh/RhSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.postalis.training.rh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
